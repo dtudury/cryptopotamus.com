81a25d32440e81656722a0aed162f11ac5e7bd5b
David Tudury <david.tudury@gmail.com>
2014-10-14 06:15:44 +0000

    just redirect

--- server.js
+++ server.js
@@ -1,18 +1,9 @@
 var http = require('http');
-var fs = require('fs');
-var path = require('path');
 
 module.exports = function(port, hostname) {
-    fs.readFile(path.join(__dirname, 'index.html'), function (err, file) {
-        http.createServer(function (req, res) {
-            if (req.url.match(/^\/(index\.html?)?$/)) {
-                res.writeHead(200, {'Content-Type': 'text/html'});
-                res.end(file);
-            } else {
-                res.writeHead(404);
-                res.end(http.STATUS_CODES[404]);
-            }
-        }).listen(port, hostname);
-        console.log('cryptopotamus.com running at http://%s:%s/', hostname, port);
-    });
+    http.createServer(function (req, res) {
+        res.writeHead(301, {"location" : "https://dtudury.github.io/cryptopotamus.com/"});
+        res.end();
+    }).listen(port, hostname);
+    console.log('cryptopotamus.com running at http://%s:%s/', hostname, port);
 }
